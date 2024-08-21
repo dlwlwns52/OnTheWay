37a05b60a851cdf3e018b39eee4ba38a9ceb29c3
JiJun Lee <dlwlwns52@naver.com>
2024-08-22 01:42:03 +0900

    1. 안드로이드 로고넣기 -> 미완     1) 기존 로고 나오고 -> 내가 설정한 로고가 나와서 두개가 중첩     2) 기존 로고는 없애고 흰색으로 배경설정
        3) 실제 기기로 앱 목록에서 크기 확인 현재 90 -> 미완
    
    2) 스레드 사용해서 사용자 위치 추적(성공)
        - 우선 백그라운드 포그라운드에 위치를 저장하는 건 성공
        - 임시로 회원가입에 위치추적 연결
        - 도와주기 다이어로그에서 '\n\n⚠️ 거래가 매칭되면 안전한 거래를 위해 \n거래 기간 동안 사용자의 위치 정보가 \n일시적으로 공유되며, 종료 시 자동 삭제됩니다.' 멘트 추가
    
         '
          메커니즘
           1. 도와주기를 하면 LocationTracker 인스턴스 생성후 리스너 실행(response, ownerClick 변경 감지)
           2. 상대방이 수락하면 ChatActions responce accepted
           3. 상대방이 지도에 들어가면 ChatActions ownerClick true
           4. 두개가 and로 되면 위치추적
          '
    
        - 도와주기시 위치 추적 거부시 불가로 설정 -o
        - 도와주기시 location true 설정 -> true면 저장 시작(스트림)
        - 마커찍기 - 마커는 로고에서 사람 자르기 - o
        - tmap vs kakaomap 생각해보기 - tmap사용 길찾기 효과도 그대로 간다.
    
    <예정>
    위치추적을 끊는 타임이 잘 정하기
        locationTracker.stopTracking(); 위치 잘 정하기
        스낵바 알림
    
    bool 값이 true인 계정은 1분마다 위치를 저장한다? -> 몇분마다 할까 -> geo 로도 해보자
    결제 요청 완료시 false 설정 or 12시간 지나면 false(기존코드 활용)
    
    파이어베이스 데이터베이스 보안설정
    시간 순서대로 넣기
    
    프로필사진
    결제하기 클릭시 결제요청해주세요 텍스트 보내기
    거래완료시 학점올리기
    twilio 넣기
    회원가입 비속어 차단
    
    포스터나 팜플렛 제작시 -> 도와주시 신청과 받는 학생의 장점  / 최소주문을 강조  -인스타용도 조사ㅎ보기
    <프로필 사진> - 출시 전 (왜냐하면 현재는 임의의 계정으로 하기때문에 실제 구글계정이 아니라 프사를 불러올 수가 없다.)
        프로필에서 현재 구글 이미지가 프로필 이미지가 된다 -> 채팅방에서도 적용
        프사 넣기 -> 이거는 구글 계정으로 로그인하면 해당 프로필 사진인데 현재는 내가 임의로 만든 게정이라 안됨
    
    #마지막으로 해야할일
    개인정보 보호규정
    앱개발 마케팅 영상
    다이소, 편의점도 가능하다는 것을 강조
    포스터나 팜플렛 제작시 -> 도와주시 신청과 받는 학생의 장점  / 최소주문을 강조
    배달업체에게도 줘보자 -> 한명 정해보기(이건 미정)
    개인정보 수집문?
    
    구체적으로 알려주자 - 신뢰감, 내신상?
    장점
    
    1. 알림 수락시 게시글 삭제, 글작성시 지도 버튼 활성화, 도와주기 텀 제한(30초), 도와주기 요청횟수 제한
    2. 다른 학교 추가시
        1. 게시판 수정 - ui
        2. schoolScores 컬렉션 도메인 문서 생성
        3. 학교랭킹 추가 - SchoolRanking.dart,
        4. 초기 중심 위치 설정 (현재 한밭대) - 게시글 작성시
        5. 회원가입시 학교 도메인 추가 - SchoolEmailDialog.dart
        6. 로고사진 스토리지에 넣기
        7. url 파이어스토어에 저장 - updateSchoolLogos()
        8. 바텀 게시판 이동 해당 학교 board 클래스작성
        9. AllUsersScreen.dart, SchoolRanking.dart, Profile.dart, 주문내역에 새로운 학교 import
        10. function index.js -> 대학별 알림 추가
        11. 회원탈퇴시 삭제할때 게시판 이름
        12. uiboard 에 boardCount 수정
    
    3. 테스트 메일 삭제 - gmail.com

---
+++ lib/test/LocationTest.dart
@@ -0,0 +1,72 @@
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class LocationTracker {
+  bool trackingEnabled = false;
+  StreamSubscription<Position>? positionStream;
+  Timer? _timer;
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String docunmetName;  // 사용자 닉네임을 저장하는 필드
+
+  // 생성자에서 닉네임을 받아서 초기화
+  LocationTracker(this.docunmetName);
+
+
+  // 위치 권한 요청
+  Future<bool> requestPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // throw Exception('위치 권한이 거부되었습니다.');
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // throw Exception('위치 권한이 영구적으로 거부되었습니다. 설정에서 권한을 수동으로 허용해주세요.');
+      return false;
+    }
+    return true;
+  }
+
+  // 위치 추적 시작
+  void startTracking() async {
+    // 이미 tracking 중이면 재실행하지 않도록 방지
+    if (_timer != null && _timer!.isActive) {
+      print("이미 위치 추적이 실행 중입니다.");
+      return;
+    }
+
+    trackingEnabled = true;
+
+    _timer = Timer.periodic(Duration(seconds: 5), (Timer t) async {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      saveLocation(position);
+    });
+  }
+
+
+
+  // 위치 데이터 저장
+  void saveLocation(Position position) async {
+    String locationString = "${position.latitude},${position.longitude}";
+    if (trackingEnabled) {
+      await _firestore.collection('ChatActions').doc(docunmetName).update({
+        'helper_location': locationString,
+        'helper_timestamp': FieldValue.serverTimestamp(),
+      });
+      print("${position.latitude}, ${position.longitude} 저장완료");
+    }
+  }
+
+  // 위치 추적 중지
+  void stopTracking() {
+    trackingEnabled = false;
+    _timer?.cancel();
+  }
+}
